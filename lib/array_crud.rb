44890e505322ad67c517aa1a4cfb1b291798fb95
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-22 02:36:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ['hi', 'hello', 'salut', 'biongiorno']
+  ['hi', 'hello', 'salut', 'buongiorno']
 end
 
 def add_element_to_end_of_array(array, element)
